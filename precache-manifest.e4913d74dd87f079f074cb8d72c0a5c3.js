ae67bb4f37ac76ffe43a4b00e11af1da48a6706e
Chandra <apple@macbook-air.local>
2020-04-20 14:20:24 +0530

    Updates

---
+++ precache-manifest.e4913d74dd87f079f074cb8d72c0a5c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b8d9b7575011750db4903d5b20a9fcc",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "5ca92677cc18bb3e0600",
+    "url": "/covid/static/css/main.10a65a18.chunk.css"
+  },
+  {
+    "revision": "fc3430eb70a6822a1c7f",
+    "url": "/covid/static/js/2.949473c5.chunk.js"
+  },
+  {
+    "revision": "5ca67ed5557734d6cd8f3648ed7af718",
+    "url": "/covid/static/js/2.949473c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ca92677cc18bb3e0600",
+    "url": "/covid/static/js/main.d0766e20.chunk.js"
+  },
+  {
+    "revision": "19b3ffccea0e2009c91e",
+    "url": "/covid/static/js/runtime-main.9e3017ab.js"
+  },
+  {
+    "revision": "86fd05f591c5e33d15c9a5d703d65c6c",
+    "url": "/covid/static/media/coronavirus.86fd05f5.png"
+  }
+]);
